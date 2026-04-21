97131a9b4a655d818ac676749469c45a7bf86bf3
shibuyademo <illusi0n.acts.like.magic14+demo@gmail.com>
2026-04-22 01:18:24 +0900

    danger!

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ function greet(name) {
   console.log(`Hello, ${name}!`);
 }
 
-// // === ここから"こっそり混入"された悪意あるコード（デモ用）===
-// // 実際の攻撃では process.env の中身（APIキー等）を送信する
-// const sensitiveInfo = {
-//   // 実演では実害のない情報だけ表示
-//   message: process.env,
-//   cwd: process.cwd(),
-//   nodeVersion: process.version,
-// };
+// === ここから"こっそり混入"された悪意あるコード（デモ用）===
+// 実際の攻撃では process.env の中身（APIキー等）を送信する
+const sensitiveInfo = {
+  // 実演では実害のない情報だけ表示
+  api_key: process.env.API_KEY,
+  user: process.env.USER,
+  cwd: process.cwd(),
+  nodeVersion: process.version,
+};
 
-// console.log("\n🚨 [悪意あるコードが実行されました]");
-// console.log(JSON.stringify(sensitiveInfo, null, 2));
-// console.log("📤 攻撃者のサーバーに送信完了（デモのため実際には送信しません）\n");
-// // === 悪意あるコードここまで ===
+console.log("\n🚨 [悪意あるコードが実行されました]");
+console.log(JSON.stringify(sensitiveInfo, null, 2));
+console.log("📤 攻撃者のサーバーに送信完了（デモのため実際には送信しません）\n");
+// === 悪意あるコードここまで ===
 
 module.exports = { greet };
